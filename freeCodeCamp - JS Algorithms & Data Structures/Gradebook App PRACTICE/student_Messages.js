d45093c42c032612ad3d36e051b17ee297f09a76
Laura <arnoldlaura24@gmail.com>
2025-07-09 17:47:20 -0700

    Create student_Messages.js

---
+++ freeCodeCamp - JS Algorithms & Data Structures/Gradebook App PRACTICE/student_Messages.js
@@ -0,0 +1,11 @@
+function studentMsg(totalScores, studentScore) {
+  const average = getAverage(totalScores);
+  const grade = getGrade(studentScore);
+  const passed = hasPassingGrade(studentScore);
+  if (passed) {
+    return "Class average: " + average + ". Your grade: " + grade + ". You passed the course.";
+  } else {
+    return "Class average: " + average + ". Your grade: " + grade + ". You failed the course.";
+  }
+}
+console.log(studentMsg([92, 88, 12, 77, 57, 100, 67, 38, 97, 89], 37));
