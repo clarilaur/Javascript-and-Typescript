69d72824d019c86529a058b13a2eb5fd61e30b39
Laura <arnoldlaura24@gmail.com>
2025-10-21 18:25:15 -0700

    Practice questions

---
+++ Interactive World Map/Basic Fundamentals/examplesPrompt_and_Console.js
@@ -0,0 +1,18 @@
+city = prompt("What city do you live in?");
+
+console.log("Fall in " + city + " is great.");
+
+_____________________________________________
+let drinkQuantity = 2;
+let tacoQuantity = 3;
+
+let totalCost = (drinkQuantity*2)+ (tacoQuantity*7);
+
+_____________________________________________
+
+let slicesInPizza = 8;
+let slicesPerGuest = 4;
+let totalPersons = 12;
+let pizzasNeeded = (totalPersons*slicesPerGuest)/slicesInPizza;
+
+
