0f1874db34967a8ec3a3ea3e98787d2cd30aa889
Laura <arnoldlaura24@gmail.com>
2025-10-21 17:58:28 -0700

    Change tvShow assignment to user prompt
    Prompt user for favorite TV show and handle null input.

--- Interactive World Map/Basic Fundamentals/if_Else_Statements.js
+++ Interactive World Map/Basic Fundamentals/if_Else_Statements.js
@@ -1,8 +1,9 @@
-let tvShow = "The Office";
+let tvShow = prompt("What is your favorite TV show?");
 
 if (tvShow === null) {
    console.log("You did not enter a TV show.");
 }
 else {
-   console.log(tvShow + " is your favorite TV show!");
+   console.log(tvShow);
 }
+
