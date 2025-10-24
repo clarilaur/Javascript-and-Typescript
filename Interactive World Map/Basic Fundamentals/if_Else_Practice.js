3b06658e1acf33eeda614ccfdeb282f70b2d472f
Laura <arnoldlaura24@gmail.com>
2025-10-23 18:14:29 -0700

    Add age check with voting eligibility messages

---
+++ Interactive World Map/Basic Fundamentals/if_Else_Practice.js
@@ -0,0 +1,13 @@
+let userAge = 18;
+if (userAge <= 12) {
+   console.log("Enjoy your early years.");
+}
+else {
+   console.log("You are at least 13.");
+   
+   if (userAge >= 18) {
+      console.log("You are old enough to vote.");
+   }
+   else {
+      console.log("You are too young to vote.");
+   }
