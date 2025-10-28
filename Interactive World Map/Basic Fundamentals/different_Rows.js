c7d8921dab6bb89b8451a7d7ecb9f7518a2b176c
Laura <arnoldlaura24@gmail.com>
2025-10-27 20:57:59 -0700

    Add different_Rows.js to display dog information

---
+++ Interactive World Map/Basic Fundamentals/different_Rows.js
@@ -0,0 +1,9 @@
+let dogName = "Max";
+let breed = "Golden Retriever";
+let age = 3;
+let isTrained = true;
+
+console.log("Meet our dog:", dogName);
+console.log("Breed:", breed);
+console.log("Age:", age, "years old");
+console.log("Trained?", isTrained);
