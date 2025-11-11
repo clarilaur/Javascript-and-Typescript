8c872f37f83d3a747adcfc7e615750087b545e50
Laura <arnoldlaura24@gmail.com>
2025-11-10 20:19:57 -0800

    Add switch statement for coin identification

---
+++ Interactive World Map/Basic Fundamentals/switch_Statement.js
@@ -0,0 +1,19 @@
+change = 10;
+switch (change) {
+   case 1:
+      coin = "penny";
+      break;
+   case 5:
+      coin = "nickel";
+      break;
+   case 10:
+      coin = "dime";
+      break;
+   case 25:
+      coin = "quarter";
+      break;
+   default:  
+      coin = "unknown";
+}
+
+console.log(coin);
