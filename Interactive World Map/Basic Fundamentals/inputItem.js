f326dcd7208df46d05bf1b4ad5aea8460d3368b7
Laura <arnoldlaura24@gmail.com>
2025-11-11 21:13:44 -0800

    Add inputItem.js with switch case for languages

---
+++ Interactive World Map/Basic Fundamentals/inputItem.js
@@ -0,0 +1,16 @@
+let inputItem = 4;
+
+switch (inputItem) {
+  case 4:
+    console.log("HTML");
+    break;
+  case 5:
+    console.log("CSS");
+    break;
+  case 6:
+    console.log("JavaScript");
+    break;
+  default:
+    console.log("PHP");
+    break;
+}
